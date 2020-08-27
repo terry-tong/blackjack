600bb8e50878b38503ba9ac9450840e1e20af4d3
terry-tong <69437642+terry-tong@users.noreply.github.com>
2020-08-27 10:37:00 -0400

    Add files via upload
    Foundational stuff. Sets up all the cards, suits, and respective shorthand representations.

---
+++ blackjack.py
@@ -0,0 +1,8 @@
+suitsalpha = ["Clubs", "Diamonds", "Hearts", "Spades"]
+suitssymbol = ["♣", "♦", "♥", "♠"]
+cardsalpha = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
+cardsnum = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+deck = cardsalpha[0] + " of " + suitsalpha[0]
+deckshort = cardsnum[0] + suitssymbol[0]
+print(deck)
+print(deckshort)
